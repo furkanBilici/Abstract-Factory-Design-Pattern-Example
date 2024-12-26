f273e1635a20816e1e7739cecbd75bf0ef656763
furkanBilici <fbilici258@gmail.com>
2024-12-26 16:21:08 +0300

    Cache örneği eklenmesi

---
+++ AbstractFactory/Cache/NCache.cs
@@ -0,0 +1,17 @@
+﻿using AbstractFactory.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Cache
+{
+    internal class NCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with NCache",data);
+        }
+    }
+}
