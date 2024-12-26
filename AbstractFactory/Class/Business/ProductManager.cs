1deec17a5e22498c740fd8ce8235726001040adc
furkanBilici <fbilici258@gmail.com>
2024-12-26 17:58:30 +0300

    productmanager update

---
+++ AbstractFactory/Class/Business/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using AbstractFactory.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Class.Business
+{
+    public class ProductManager
+    {
+        CrossCuttingConsernsFactory _crossCuttingConsernsFactory;
+        public ProductManager(CrossCuttingConsernsFactory crossCuttingConsernsFactory) //bunun sayesinde cccfactory'si abstract edinen tüm classlar gönderilip çağrılabilir
+        {
+            _crossCuttingConsernsFactory = crossCuttingConsernsFactory;
+        }
+        public void GetAll()
+        {
+            //ek kodlar olabilir
+            Console.WriteLine("Here:...");
+        }
+    }
+}
